7f7096866853dca23ec42f03a5b84ca6d4ffaea6
Yannick Stachelscheid <yannick.stachelscheid@tum.de>
2020-09-12 11:01:49 +0200

    add initial schema version and apply

--- src/database-schema.ts
+++ src/database-schema.ts
@@ -1,23 +1,33 @@
-import { IDatabaseClient, IDatabaseBaseClient } from './database-client'
-import { TableSchema, ColumnType, NativeFunction, Table } from './table';
-import { SQL } from './sql';
+import { IDatabaseClient, IDatabaseBaseClient } from "./database-client"
+import { TableSchema, ColumnType, NativeFunction, Table } from "./table"
+import { SQL } from "./sql"
 
 const schema_management = TableSchema({
-    name: { type: ColumnType.Varchar, primaryKey: true, nullable: false, unique: true },
-    version: { type: ColumnType.Integer, nullable: false },
-    date_added: { type: ColumnType.TimestampTZ, nullable: false, defaultValue: { func: NativeFunction.Now } },
-    locked: { type: ColumnType.Boolean, nullable: false, defaultValue: false },
+	name: {
+		type: ColumnType.Varchar,
+		primaryKey: true,
+		nullable: false,
+		unique: true,
+	},
+	version: { type: ColumnType.Integer, nullable: false },
+	date_added: {
+		type: ColumnType.TimestampTZ,
+		nullable: false,
+		defaultValue: { func: NativeFunction.Now },
+	},
+	locked: { type: ColumnType.Boolean, nullable: false, defaultValue: false },
 })
-const SchemaManagementTable = Table({ schema_management }, 'schema_management')
+const SchemaManagementTable = Table({ schema_management }, "schema_management")
 
-const selectVersionQuery = (name: string) => SchemaManagementTable.select('*', ['name'])([name])
+const selectVersionQuery = (name: string) => SchemaManagementTable.select("*", ["name"])([name])
 const insertSchemaQuery = (name: string, version: number) => SchemaManagementTable.insertFromObj({ name, version })
-const updateSchemaVersionQuery = (name: string, newVersion: number) => SchemaManagementTable.update(['version'], ['name'])([newVersion], [name])
+const updateSchemaVersionQuery = (name: string, newVersion: number) =>
+	SchemaManagementTable.update(["version"], ["name"])([newVersion], [name])
 
 export type IDatabaseSchema = ReturnType<typeof DatabaseSchema>
 
 export interface IMigration {
-    up: (client: IDatabaseBaseClient) => Promise<void>;
+	up: (client: IDatabaseBaseClient) => Promise<void>
 }
 
 export const Migration = (up: (client: IDatabaseBaseClient) => Promise<void>): IMigration => ({ up })
@@ -25,137 +35,157 @@ export const Migration = (up: (client: IDatabaseBaseClient) => Promise<void>): I
 export type CreateStatement = string
 
 export interface IDatabaseSchemaArgs {
-    name: string;
-    client: IDatabaseClient;
-    createStatements: CreateStatement[];
-    migrations: Map<number, IMigration>;
-    logMigrations?: boolean;
+	name: string
+	version: number
+	client: IDatabaseClient
+	createStatements: CreateStatement[]
+	migrations: Map<number, IMigration>
+	logMigrations?: boolean
 }
 
-export const DatabaseSchema = ({ client, createStatements, name, migrations, logMigrations }: IDatabaseSchemaArgs) => {
-    let version = 0
-    let isInitialized = false
+export const DatabaseSchema = ({
+	client,
+	createStatements,
+	name,
+	migrations,
+	logMigrations,
+	version: initialVersion,
+}: IDatabaseSchemaArgs) => {
+	let version = 0
+	let isInitialized = false
 
-    const init = async () => {
-        if (isInitialized) {
-            throw new Error(`Database schema ${name} has already been initialized.`)
-        }
+	const init = async () => {
+		if (isInitialized) {
+			throw new Error(`Database schema ${name} has already been initialized.`)
+		}
 
-        try {
-            await client.transaction(async (transaction) => {
-                await transaction.query(SchemaManagementTable.create())
+		try {
+			await client.transaction(async (transaction) => {
+				await transaction.query(SchemaManagementTable.create())
 
-                const versionDBResults = await transaction.query(selectVersionQuery(name))
+				const versionDBResults = await transaction.query(selectVersionQuery(name))
 
-                if (versionDBResults.length === 0) {
-                    await transaction.query({
-                        sql: createStatements.join('\n'),
-                    })
-                    await transaction.query(insertSchemaQuery(name, 1))
+				if (versionDBResults.length === 0) {
+					await transaction.query({
+						sql: createStatements.join("\n"),
+					})
+					await transaction.query(insertSchemaQuery(name, initialVersion))
 
-                    version = 1
-                } else {
-                    version = versionDBResults[0].version
-                }
-            })
-        } catch (err) {
-            if (err.message.indexOf('duplicate key value violates unique constraint') === -1) {
-                throw err
-            }
-        }
+					version = initialVersion
+				} else {
+					version = versionDBResults[0].version
+				}
+			})
+		} catch (err) {
+			if (err.message.indexOf("duplicate key value violates unique constraint") === -1) {
+				throw err
+			}
+		}
 
-        isInitialized = true
-    }
+		isInitialized = true
+	}
 
-    const throwNotInitialized = () => {
-        throw new Error(`Migration failed, database schema is not initialized. Please call init() first on your database schema.`)
-    }
+	const throwNotInitialized = () => {
+		throw new Error(
+			`Migration failed, database schema is not initialized. Please call init() first on your database schema.`,
+		)
+	}
 
-    const lockSchemaTableQuery = SQL.raw(`
+	const lockSchemaTableQuery = SQL.raw(
+		`
                 LOCK TABLE ${SchemaManagementTable.name} IN ACCESS EXCLUSIVE MODE;
-            `, [])
-    const getSchemaVersionQuery = (awaitLock: boolean) => SQL.raw<typeof schema_management>(`
+            `,
+		[],
+	)
+	const getSchemaVersionQuery = (awaitLock: boolean) =>
+		SQL.raw<typeof schema_management>(
+			`
                 SELECT * FROM ${SchemaManagementTable.name}
-                WHERE name = $1 ${!awaitLock ? 'FOR UPDATE NOWAIT' : ''};
-            `, [name])
-    const setSchemaLockQuery = (locked: boolean) => SQL.raw(`
+                WHERE name = $1 ${!awaitLock ? "FOR UPDATE NOWAIT" : ""};
+            `,
+			[name],
+		)
+	const setSchemaLockQuery = (locked: boolean) =>
+		SQL.raw(
+			`
                 UPDATE ${SchemaManagementTable.name} SET locked = $1 WHERE name=$2;
-            `, [locked, name])
+            `,
+			[locked, name],
+		)
 
-    /*
+	/*
         Locks schema_management table for given transaction and retrievs current schema version
         If table is already locked, the postgres client is advised to await execution until lock is released
         This ensures, that in a multi-node environment all starting nodes proceed code execution after all migrations are done
     */
-    const getCurrentVersionAndLockSchema = async (client: IDatabaseBaseClient, awaitLock: boolean) => {
-        await client.query(lockSchemaTableQuery)
-        const dbResults = await client.query(getSchemaVersionQuery(awaitLock))
+	const getCurrentVersionAndLockSchema = async (client: IDatabaseBaseClient, awaitLock: boolean) => {
+		await client.query(lockSchemaTableQuery)
+		const dbResults = await client.query(getSchemaVersionQuery(awaitLock))
 
-        if (dbResults.length === 1 && dbResults[0].locked === false) {
-            await client.query(setSchemaLockQuery(true))
+		if (dbResults.length === 1 && dbResults[0].locked === false) {
+			await client.query(setSchemaLockQuery(true))
 
-            return dbResults[0].version
-        }
+			return dbResults[0].version
+		}
 
-        return null
-    }
+		return null
+	}
 
-    const migrateToVersion = async (targetVersion: number) => {
-        if (!isInitialized) throwNotInitialized()
+	const migrateToVersion = async (targetVersion: number) => {
+		if (!isInitialized) throwNotInitialized()
 
-        if (targetVersion <= 1) {
-            throw new Error('Target version of migrateToVersion() has to be greater 1')
-        }
+		if (targetVersion <= 1) {
+			throw new Error("Target version of migrateToVersion() has to be greater 1")
+		}
 
-        for (let newVersion = version; newVersion <= targetVersion; newVersion++) {
-            await client.transaction(async (transaction) => {
-                const currentVersion = await getCurrentVersionAndLockSchema(transaction, true)
+		for (let newVersion = version; newVersion <= targetVersion; newVersion++) {
+			await client.transaction(async (transaction) => {
+				const currentVersion = await getCurrentVersionAndLockSchema(transaction, true)
 
-                if (currentVersion === null || currentVersion >= newVersion) {
-                    if (currentVersion) {
-                        await transaction.query(setSchemaLockQuery(false))
-                    }
+				if (currentVersion === null || currentVersion >= newVersion) {
+					if (currentVersion) {
+						await transaction.query(setSchemaLockQuery(false))
+					}
 
-                    return
-                }
+					return
+				}
 
-                const migration = migrations.get(newVersion)
+				const migration = migrations.get(newVersion)
 
-                if (!migration) {
-                    await transaction.query(setSchemaLockQuery(false))
+				if (!migration) {
+					await transaction.query(setSchemaLockQuery(false))
 
-                    throw new Error(`Migration with version ${newVersion} not found. Aborting migration process...`)
-                }
+					throw new Error(`Migration with version ${newVersion} not found. Aborting migration process...`)
+				}
 
-                await migration.up(transaction)
-                await transaction.query(updateSchemaVersionQuery(name, newVersion))
-                await transaction.query(setSchemaLockQuery(false))
+				await migration.up(transaction)
+				await transaction.query(updateSchemaVersionQuery(name, newVersion))
+				await transaction.query(setSchemaLockQuery(false))
 
-                // istanbul ignore next
-                if (!(logMigrations === false)) {
-                    console.info(`Successfully migrated ${name} from version ${version} to ${newVersion}`)
-                }
-            })
+				// istanbul ignore next
+				if (!(logMigrations === false)) {
+					console.info(`Successfully migrated ${name} from version ${version} to ${newVersion}`)
+				}
+			})
 
-            version = newVersion
-        }
-    }
+			version = newVersion
+		}
+	}
 
-    const migrateLatest = async () => {
-        const sortedMigrationKeys = Array.from(migrations.keys()).sort()
-        const latestVersion = sortedMigrationKeys[sortedMigrationKeys.length - 1]
+	const migrateLatest = async () => {
+		const sortedMigrationKeys = Array.from(migrations.keys()).sort()
+		const latestVersion = sortedMigrationKeys[sortedMigrationKeys.length - 1]
 
-        await migrateToVersion(latestVersion)
-    }
+		await migrateToVersion(latestVersion)
+	}
 
-    const getVersion = () => version
+	const getVersion = () => version
 
-    return {
-        name,
-        getVersion,
-        init,
-        migrateLatest,
-        migrateToVersion,
-    }
+	return {
+		name,
+		getVersion,
+		init,
+		migrateLatest,
+		migrateToVersion,
+	}
 }
-
