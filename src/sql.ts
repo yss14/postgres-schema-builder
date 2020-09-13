bba698acebfaa84bf9e3599f751e921cb96c5e55
Yannick Stachelscheid <yannickstach@googlemail.com>
2020-09-13 15:04:02 +0200

    fix: various migration issues (#251)
    * add prettier config
    * pass transaction and database client to migrations
    * take initial schema version from migrations (max value)
    * version 1.2.0-beta.1

--- src/sql.ts
+++ src/sql.ts
@@ -1,70 +1,90 @@
-import { Columns, Column, IReferenceConstraintInternal, isCollection, isSQLFunction, ForeignKeyUpdateDeleteRule, ICreateIndexStatement, IQuery, isJSONType, IWhereConditionColumned, ISQLArg } from "./table";
-import * as pgEscape from 'pg-escape';
-import { dateToSQLUTCFormat } from "./sql-utils";
-import * as moment from 'moment'
-import { flatten } from './utils'
+import {
+	Columns,
+	Column,
+	IReferenceConstraintInternal,
+	isCollection,
+	isSQLFunction,
+	ForeignKeyUpdateDeleteRule,
+	ICreateIndexStatement,
+	IQuery,
+	isJSONType,
+	IWhereConditionColumned,
+	ISQLArg,
+} from "./table"
+import pgEscape from "pg-escape"
+import { dateToSQLUTCFormat } from "./sql-utils"
+import moment from "moment"
+import { flatten } from "./utils"
 
-const isStringArray = (arr: any): arr is string[] => Array.isArray(arr) && arr.every(item => typeof item === 'string')
+const isStringArray = (arr: any): arr is string[] => Array.isArray(arr) && arr.every((item) => typeof item === "string")
 
 export namespace SQL {
-	export const createDatabase = (name: string) => `CREATE DATABASE ${name};`;
+	export const createDatabase = (name: string) => `CREATE DATABASE ${name};`
 
 	export const createTable = (name: string, columns: Columns): string => {
-		const entries = Object.entries(columns).map(([name, column]) => ({ name, ...column }));
-		const foreignKeyConstraints: IReferenceConstraintInternal[] = collectForeignKeyConstraints(entries);
+		const entries = Object.entries(columns).map(([name, column]) => ({ name, ...column }))
+		const foreignKeyConstraints: IReferenceConstraintInternal[] = collectForeignKeyConstraints(entries)
 
-		const primaryKeyColoumns = entries.filter(col => {
-			return col.primaryKey !== undefined && col.primaryKey;
-		});
+		const primaryKeyColoumns = entries.filter((col) => {
+			return col.primaryKey !== undefined && col.primaryKey
+		})
 
 		if (primaryKeyColoumns.length === 0) {
-			throw new Error(`Primary Key(s) missing. Cannot create table ${name}.`);
+			throw new Error(`Primary Key(s) missing. Cannot create table ${name}.`)
 		}
 
 		const createTableQuery = `
                 CREATE TABLE IF NOT EXISTS ${name} (
 				${entries
-				.map(prepareCreateColumnStatement)
-				.concat([
-					`CONSTRAINT PK_${name}_${primaryKeyColoumns.map(pkc => pkc.name).join('_')} PRIMARY KEY (${primaryKeyColoumns.map(pkc => `"${pkc.name}"`).join(',')})`,
-				])
-				.concat(
-					prepareForeignKeyConstraintStatements(name, foreignKeyConstraints)
-						.map(stmt => `CONSTRAINT ${stmt}`)
-				)
-				.join(',\n')}
+					.map(prepareCreateColumnStatement)
+					.concat([
+						`CONSTRAINT PK_${name}_${primaryKeyColoumns
+							.map((pkc) => pkc.name)
+							.join("_")} PRIMARY KEY (${primaryKeyColoumns.map((pkc) => `"${pkc.name}"`).join(",")})`,
+					])
+					.concat(
+						prepareForeignKeyConstraintStatements(name, foreignKeyConstraints).map(
+							(stmt) => `CONSTRAINT ${stmt}`,
+						),
+					)
+					.join(",\n")}
             );
-		`;
+		`
 
-		const indexStatements: ICreateIndexStatement[] = entries.filter((col): boolean => {
-			return (col.unique !== undefined && col.unique) || (col.createIndex !== undefined && col.createIndex);
-		}).map((col) => {
-			return {
-				column: col.name,
-				unique: col.unique !== undefined && col.unique,
-			}
-		});
+		const indexStatements: ICreateIndexStatement[] = entries
+			.filter((col): boolean => {
+				return (col.unique !== undefined && col.unique) || (col.createIndex !== undefined && col.createIndex)
+			})
+			.map((col) => {
+				return {
+					column: col.name,
+					unique: col.unique !== undefined && col.unique,
+				}
+			})
 
-		const createIndexQueries = indexStatements.map(indexStatement => createIndex(indexStatement.unique, name, indexStatement.column));
+		const createIndexQueries = indexStatements.map((indexStatement) =>
+			createIndex(indexStatement.unique, name, indexStatement.column),
+		)
 
 		return `
 			${createTableQuery}
-			${createIndexQueries.join('\n')}
-		`;
+			${createIndexQueries.join("\n")}
+		`
 	}
 
 	export const addColumns = (tableName: string, columns: Columns): string => {
-		const entries = Object.entries(columns).map(([name, column]) => ({ name, ...column }));
-		const foreignKeyConstraints: IReferenceConstraintInternal[] = collectForeignKeyConstraints(entries);
+		const entries = Object.entries(columns).map(([name, column]) => ({ name, ...column }))
+		const foreignKeyConstraints: IReferenceConstraintInternal[] = collectForeignKeyConstraints(entries)
 
 		const addForeignKeyConstraintsStatements = `
 			${prepareForeignKeyConstraintStatements(tableName, foreignKeyConstraints)
-				.map(constraint => `ALTER TABLE ${tableName} ADD CONSTRAINT ${constraint}`).join(';\n')}
+				.map((constraint) => `ALTER TABLE ${tableName} ADD CONSTRAINT ${constraint}`)
+				.join(";\n")}
 		`
 
 		const addTableColumnStatement = `
 			ALTER TABLE ${tableName}
-			${entries.map(entry => `ADD COLUMN ${prepareCreateColumnStatement(entry)}`).join(',\n')};
+			${entries.map((entry) => `ADD COLUMN ${prepareCreateColumnStatement(entry)}`).join(",\n")};
 		`
 
 		return `
@@ -74,33 +94,35 @@ export namespace SQL {
 	}
 
 	type DropColumns = {
-		(tableName: string, columns: Columns): string;
-		(tableName: string, columns: string[], constraints?: string[]): string;
+		(tableName: string, columns: Columns): string
+		(tableName: string, columns: string[], constraints?: string[]): string
 	}
 
-	export const dropColumns: DropColumns = (tableName: string, columns: Columns | string[], constraints?: string[]): string => {
-		const columnNames = isStringArray(columns)
-			? columns
-			: Object.keys(columns)
+	export const dropColumns: DropColumns = (
+		tableName: string,
+		columns: Columns | string[],
+		constraints?: string[],
+	): string => {
+		const columnNames = isStringArray(columns) ? columns : Object.keys(columns)
 		let constraintNames: string[] = []
 
 		if (constraints && constraints.length > 0) {
 			constraintNames = constraints
 		} else if (!isStringArray(columns)) {
-			const entries = Object.entries(columns).map(([name, column]) => ({ name, ...column }));
+			const entries = Object.entries(columns).map(([name, column]) => ({ name, ...column }))
 			const foreignKeyConstraints = collectForeignKeyConstraints(entries)
 
-			constraintNames = foreignKeyConstraints.map(fkc => `${tableName}_${fkc.column}_fkey`)
+			constraintNames = foreignKeyConstraints.map((fkc) => `${tableName}_${fkc.column}_fkey`)
 		}
 
 		const dropTableColumnsStatement = `
 			ALTER TABLE ${tableName}
-			${columnNames.map(column => `DROP COLUMN ${column}`).join(',\n')};
+			${columnNames.map((column) => `DROP COLUMN ${column}`).join(",\n")};
 		`
 
 		const dropConstraintsStatement = constraintNames
-			.map(constraint => `ALTER TABLE ${tableName} DROP CONSTRAINT ${constraint};`)
-			.join('\n')
+			.map((constraint) => `ALTER TABLE ${tableName} DROP CONSTRAINT ${constraint};`)
+			.join("\n")
 
 		return `
 			${dropConstraintsStatement}
@@ -109,30 +131,31 @@ export namespace SQL {
 	}
 
 	export const insert = (tableName: string, subset: string[]) => {
-		const cql = `INSERT INTO ${tableName}`
-			+ ` ( ${subset.map(column => `"${column}"`).join(", ")} )`
-			+ ` VALUES ( ${subset.map((_, idx) => `$${idx + 1}`).join(', ')} );`;
+		const cql =
+			`INSERT INTO ${tableName}` +
+			` ( ${subset.map((column) => `"${column}"`).join(", ")} )` +
+			` VALUES ( ${subset.map((_, idx) => `$${idx + 1}`).join(", ")} );`
 
-		return cql;
+		return cql
 	}
 
 	export const update = (tableName: string, subset: string[], where: string[]) => {
-		const cql = `UPDATE ${tableName} `
-			+ `SET ${subset.map((col, idx) => `"${col}" = $${idx + 1}`).join(', ')} `
-			+ `WHERE ${where.map((col, idx) => `"${col}" = $${subset.length + idx + 1}`).join(' AND ')};`
+		const cql =
+			`UPDATE ${tableName} ` +
+			`SET ${subset.map((col, idx) => `"${col}" = $${idx + 1}`).join(", ")} ` +
+			`WHERE ${where.map((col, idx) => `"${col}" = $${subset.length + idx + 1}`).join(" AND ")};`
 
-		return cql;
+		return cql
 	}
 
-	export const selectAll = (tableName: string, subset: string[] | '*') => {
-		const cql = `SELECT ${subset === '*' ? '*' : subset.join(', ')} `
-			+ `FROM ${tableName};`;
+	export const selectAll = (tableName: string, subset: string[] | "*") => {
+		const cql = `SELECT ${subset === "*" ? "*" : subset.join(", ")} ` + `FROM ${tableName};`
 
-		return cql;
+		return cql
 	}
 
 	const whereConditionToString = (cond: string | ISQLArg, idx: number) => {
-		if (typeof cond === 'string') {
+		if (typeof cond === "string") {
 			return `("${cond}" = $${idx + 1})`
 		}
 
@@ -140,109 +163,127 @@ export namespace SQL {
 	}
 
 	type Select = {
-		(tableName: string, subset: string[] | '*', where: string[]): string;
-		(tableName: string, subset: string[] | '*', where: ISQLArg[]): string;
+		(tableName: string, subset: string[] | "*", where: string[]): string
+		(tableName: string, subset: string[] | "*", where: ISQLArg[]): string
 	}
 
-	export const select: Select = (tableName: string, subset: string[] | '*', where: (string | IWhereConditionColumned)[]) => {
-		const cql = `SELECT ${subset === "*" ? "*" : subset.map(column => `"${column}"`).join(", ")}`
-			+ ` FROM ${tableName}`
-			+ ` WHERE ${where.map(whereConditionToString).join(' AND ')}`
-			+ `;`;
+	export const select: Select = (
+		tableName: string,
+		subset: string[] | "*",
+		where: (string | IWhereConditionColumned)[],
+	) => {
+		const cql =
+			`SELECT ${subset === "*" ? "*" : subset.map((column) => `"${column}"`).join(", ")}` +
+			` FROM ${tableName}` +
+			` WHERE ${where.map(whereConditionToString).join(" AND ")}` +
+			`;`
 
-		return cql;
+		return cql
 	}
 
 	export const deleteEntry = (tableName: string, where: string[]) => {
-		const cql = `DELETE`
-			+ ` FROM ${tableName}`
-			+ ` WHERE ${where.map((column, i) => `("${column}" = $${i + 1})`).join(' AND ')}`
-			+ `;`;
+		const cql =
+			`DELETE` +
+			` FROM ${tableName}` +
+			` WHERE ${where.map((column, i) => `("${column}" = $${i + 1})`).join(" AND ")}` +
+			`;`
 
-		return cql;
+		return cql
 	}
 
-	export const dropTable = (tableName: string) => `DROP TABLE ${tableName};`;
+	export const dropTable = (tableName: string) => `DROP TABLE ${tableName};`
 
 	export const createIndex = (unique: boolean, name: string, column: string): string => {
-		const sql = `CREATE ${unique ? 'UNIQUE ' : ''}INDEX IF NOT EXISTS ${name}_${column}_${unique ? 'u' : ''}index ON ${name} (${column});`
+		const sql = `CREATE ${unique ? "UNIQUE " : ""}INDEX IF NOT EXISTS ${name}_${column}_${
+			unique ? "u" : ""
+		}index ON ${name} (${column});`
 
-		return sql;
+		return sql
 	}
 
 	export const raw = <T extends {}>(sql: string, values: unknown[] = []): IQuery<T> => ({
 		sql,
 		values,
-	});
+	})
 }
 
 const collectForeignKeyConstraints = (columns: ({ name: string } & Column)[]): IReferenceConstraintInternal[] => {
 	return flatten(
-		columns.map(col => col.foreignKeys
-			? col.foreignKeys.map(fkc => ({ ...fkc, column: col.name }))
-			: []
-		)
+		columns.map((col) => (col.foreignKeys ? col.foreignKeys.map((fkc) => ({ ...fkc, column: col.name })) : [])),
 	)
 }
 
-const prepareCreateColumnStatement = (col: ({ name: string } & Column)): string => {
-	const replaceArr: any[] = [];
+const prepareCreateColumnStatement = (col: { name: string } & Column): string => {
+	const replaceArr: any[] = []
 
 	if (col.defaultValue !== undefined) {
-		replaceArr.push(col.defaultValue);
+		replaceArr.push(col.defaultValue)
 	}
 
-	return `"${col.name}" ${!col.autoIncrement ? mapColumnType(col) : ''} ` +
-		`${col.autoIncrement ? 'SERIAL ' : ''}` +
-		`${col.nullable !== undefined && !col.nullable ? 'NOT NULL ' : ''}` +
-		`${col.defaultValue !== undefined ? `DEFAULT ${isSQLFunction(col.defaultValue) ?
-			col.defaultValue.func : mapValues(col.defaultValue)}` : ''}`;
+	return (
+		`"${col.name}" ${!col.autoIncrement ? mapColumnType(col) : ""} ` +
+		`${col.autoIncrement ? "SERIAL " : ""}` +
+		`${col.nullable !== undefined && !col.nullable ? "NOT NULL " : ""}` +
+		`${
+			col.defaultValue !== undefined
+				? `DEFAULT ${isSQLFunction(col.defaultValue) ? col.defaultValue.func : mapValues(col.defaultValue)}`
+				: ""
+		}`
+	)
 }
 
-const prepareForeignKeyConstraintStatements = (tableName: string, foreignKeyConstraints: IReferenceConstraintInternal[]): string[] => {
-	return foreignKeyConstraints
-		.map(fkc =>
+const prepareForeignKeyConstraintStatements = (
+	tableName: string,
+	foreignKeyConstraints: IReferenceConstraintInternal[],
+): string[] => {
+	return foreignKeyConstraints.map(
+		(fkc) =>
 			`${tableName}_${fkc.column}_fkey
 			FOREIGN KEY (${fkc.column}) REFERENCES ${fkc.targetTable} (${fkc.targetColumn})
-			${fkc.onDelete !== undefined ? mapUpdateDeleteRule(fkc.onDelete, false) : ''}
-			${fkc.onUpdate !== undefined ? mapUpdateDeleteRule(fkc.onUpdate, true) : ''}`
-		);
+			${fkc.onDelete !== undefined ? mapUpdateDeleteRule(fkc.onDelete, false) : ""}
+			${fkc.onUpdate !== undefined ? mapUpdateDeleteRule(fkc.onUpdate, true) : ""}`,
+	)
 }
 
 const mapColumnType = (col: Column) => {
 	if (isJSONType(col.type)) {
-		return 'JSON';
+		return "JSON"
 	} else if (isCollection(col.type)) {
-		return col.type.type.toUpperCase() + '[]';
+		return col.type.type.toUpperCase() + "[]"
 	} else {
-		return col.type.toUpperCase();
+		return col.type.toUpperCase()
 	}
 }
 
 const mapValues = (val: any): any => {
 	if (val === undefined || val === null) {
-		return 'NULL';
-	} else if (typeof val === 'string') {
-		return pgEscape('%L', val);
+		return "NULL"
+	} else if (typeof val === "string") {
+		return pgEscape("%L", val)
 	} else if (moment.isMoment(val)) {
-		return `'${dateToSQLUTCFormat(val.utc().toDate())}'`;
+		return `'${dateToSQLUTCFormat(val.utc().toDate())}'`
 	} else if (val instanceof Date) {
-		return `'${dateToSQLUTCFormat(val)}'`;
-	} else if (typeof val === 'object') {
-		return mapValues(JSON.stringify(val));
+		return `'${dateToSQLUTCFormat(val)}'`
+	} else if (typeof val === "object") {
+		return mapValues(JSON.stringify(val))
 	} else {
-		return val;
+		return val
 	}
 }
 
 const mapUpdateDeleteRule = (rule: ForeignKeyUpdateDeleteRule, isUpdate: boolean): string => {
-	const prefix = isUpdate ? 'UPDATE' : 'DELETE';
+	const prefix = isUpdate ? "UPDATE" : "DELETE"
 
 	switch (rule) {
-		case ForeignKeyUpdateDeleteRule.Cascade: return `ON ${prefix} CASCADE`;
-		case ForeignKeyUpdateDeleteRule.NoAction: return '';
-		case ForeignKeyUpdateDeleteRule.Restrict: return '';
-		case ForeignKeyUpdateDeleteRule.SetDefault: return `ON ${prefix} SET DEFAULT`;
-		case ForeignKeyUpdateDeleteRule.SetNull: return `ON ${prefix} SET NULL`;
+		case ForeignKeyUpdateDeleteRule.Cascade:
+			return `ON ${prefix} CASCADE`
+		case ForeignKeyUpdateDeleteRule.NoAction:
+			return ""
+		case ForeignKeyUpdateDeleteRule.Restrict:
+			return ""
+		case ForeignKeyUpdateDeleteRule.SetDefault:
+			return `ON ${prefix} SET DEFAULT`
+		case ForeignKeyUpdateDeleteRule.SetNull:
+			return `ON ${prefix} SET NULL`
 	}
 }
